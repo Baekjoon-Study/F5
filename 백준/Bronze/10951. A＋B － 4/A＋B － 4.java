a8e57fe379439ee09cb984d13122a98b8553ee31
CH-J-H-P <1004skfro7@naver.com>
2024-07-16 13:32:34 +0900

    [Bronze V] Title: A+B - 4, Time: 224 ms, Memory: 17968 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args)throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNextInt()) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            System.out.println(A + B);
+        }
+        
+        sc.close();
+    }
+}

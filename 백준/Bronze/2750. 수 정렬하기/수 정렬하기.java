943f381c007cb678a9f6983f1f2526c6a213504a
CH-J-H-P <1004skfro7@naver.com>
2024-08-02 19:49:19 +0900

    [Bronze II] Title: 수 정렬하기, Time: 352 ms, Memory: 22412 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args)throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] inu = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            inu[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                if(inu[i] > inu[j]){
+                    int temp = inu[i];
+                    inu[i] = inu[j];
+                    inu[j] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            System.out.println(inu[i]);
+        }
+
+        sc.close();
+    }
+}
